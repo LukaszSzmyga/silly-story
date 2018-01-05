840bb3f91be11f6ab61bf41d15f26a953c94232b
Lukasz Szmyga <lukasz.szmyga@gmail.com>
2018-01-05 17:44:37 +0100

    silly story

--- scripts/main.js
+++ scripts/main.js
@@ -21,10 +21,9 @@ function result() {
   var yItem = randomValueFromArray(insertY);
   var zItem = randomValueFromArray(insertZ);
 
-  newStory = newStory.replace(':insertx:',xItem);
-  newStory = newStory.replace(':insertx:',xItem);
-  newStory = newStory.replace(':inserty:',yItem);
-  newStory = newStory.replace(':insertz:',zItem);
+  newStory = newStory.replace(/:insertx:/g, xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
   
   if(customName.value != '') {
     var name = customName.value;
@@ -43,3 +42,4 @@ function result() {
   story.style.visibility = 'visible';
 }
 
+document.addEventListener(“DOMContentLoaded”, main);
